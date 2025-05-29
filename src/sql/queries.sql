1ca9cc7627e5cb9a280512964460eca26226e23b
ViceVareles <vicente_vareles@hotmail.com>
2025-05-29 12:24:59 +0000

    Proyecto terminado

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -16,7 +16,35 @@ JOIN species ON observations.species_id = species.id
 JOIN regions ON observations.region_id = regions.id
 GROUP BY regions.name, regions.country
 ORDER BY total_species DESC;
-SELECT * FROM observations LIMIT 6;
-SELECT CONVERT(observation_date, 120) AS formatted_date
+SELECT strftime('%m',observation_date) AS mes FROM observations
+GROUP BY mes ORDER BY COUNT(*) DESC;
+SELECT scientific_name, common_name, SUM(count) AS viewings FROM species 
+INNER JOIN observations ON species.id = observations.species_id 
+GROUP BY species_id ORDER BY viewings ASC;
+SELECT 
+    regions.name, 
+    regions.country,
+    COUNT(DISTINCT species.id) AS total_species
 FROM observations
-LIMIT 6;
+JOIN species ON observations.species_id = species.id
+JOIN regions ON observations.region_id = regions.id
+GROUP BY regions.name, regions.country
+ORDER BY total_species DESC
+LIMIT 1;
+SELECT scientific_name, common_name, COUNT(count) AS viewings FROM species 
+INNER JOIN observations ON species.id = observations.species_id WHERE count > 0
+GROUP BY species_id ORDER BY viewings DESC;
+SELECT observer, COUNT(*) AS registers FROM observations
+GROUP BY observer ORDER BY registers DESC;
+SELECT scientific_name, common_name FROM species 
+LEFT JOIN observations ON species.id = observations.species_id 
+WHERE count IS NULL OR count = 0
+GROUP BY species.id;
+SELECT observation_date FROM observations
+WHERE count > 0 GROUP BY observation_date 
+ORDER BY COUNT(*) DESC;
+
+
+
+
+
