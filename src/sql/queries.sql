ca8abb314c848df8b9147c26043fa549af524f3c
ViceVareles <vicente_vareles@hotmail.com>
2025-05-29 10:09:33 +0000

    PRIMERO TERMINADO POR FIN

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -7,11 +7,8 @@ SELECT * FROM species;
 SELECT * FROM climate;
 SELECT * FROM observations;
 
-
--- MISSION 1
--- Your query here
 SELECT 
-    regions.name,
+    regions.name, 
     regions.country,
     COUNT(DISTINCT species.id) AS total_species
 FROM observations
@@ -19,40 +16,7 @@ JOIN species ON observations.species_id = species.id
 JOIN regions ON observations.region_id = regions.id
 GROUP BY regions.name, regions.country
 ORDER BY total_species DESC;
-
--- MISSION 2
--- Your query here:
--- lo revisado en grupos
--- MISSION 1
--- Your query here;
-SELECT name, country FROM regions
-INNER JOIN observations ON regions.id = observations.region_id
-WHERE count > 0 GROUP BY region_id ORDER BY COUNT(*) DESC;
-
--- MISSION 2
--- Your query here;
-SELECT strftime('%m',observation_date) AS mes FROM observations
-GROUP BY mes ORDER BY COUNT(count) DESC;
-
--- MISSION 3
--- Your query here:
-
-
--- MISSION 4
--- Your query here:
-
-
--- MISSION 5
--- Your query here:
-
-
--- MISSION 6
--- Your query here:
-
-
--- MISSION 7
--- Your query here:
-
-
--- MISSION 8
--- Your query here:
+SELECT * FROM observations LIMIT 6;
+SELECT CONVERT(observation_date, 120) AS formatted_date
+FROM observations
+LIMIT 6;
